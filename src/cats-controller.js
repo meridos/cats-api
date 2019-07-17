e281ab3d1ccf5aa84af448dd2868f3e3ca37295e
Nasyalnik <nasyalnik@mail.ru>
2019-07-17 11:25:43 +0300

    delete name file

--- src/cats-controller.js
+++ src/cats-controller.js
@@ -154,6 +154,28 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// function trimSymbols(name) {
+//   let startIndex = 0;
+//   for (let i = 0; i < name.length; i++) {
+//     const symbol = name[i];
+//     if (/[a-zA-Zа-яА-Я]/.test(symbol)) {
+//       startIndex = i;
+//       break;
+//     }
+//   }
+
+//   let endIndex = 0;
+//   for (let i = name.length - 1; i >= 0; i--) {
+//     const symbol = name[i];
+//     if (/[a-zA-Z1-9а-яА-Я]/.test(symbol)) {
+//       endIndex = i;
+//       break;
+//     }
+//   }
+
+//   return name.substring(startIndex, endIndex + 1);
+// }
+
 module.exports = {
   searchCatsByName,
   addCats,
